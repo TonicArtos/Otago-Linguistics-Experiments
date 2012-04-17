0fb98e7578e5a6e8dcb0424943192179b69f8f06
Hunter Hatfield <hunter.hatfield@gmail.com>
2012-04-18 11:03:12 +1200

    Set conditions to "Tutorial" for tutorial sentences as a marker.

--- src/nz/ac/otago/linguistics/spre/SentenceResult.java
+++ src/nz/ac/otago/linguistics/spre/SentenceResult.java
@@ -13,8 +13,8 @@ public class SentenceResult implements ExperimentActivity.Result {
 	public int condition1;
 	public int condition2;
 	protected Vector<Click> clicks = new Vector<Click>();
-	private static final String[] condition1Text = new String[] { "Filler", "Relative Clause", "Adverb", "Coordination" };
-	public static final String[] condition2Text = new String[] { "Filler", "High Attachment", "Low Attachment", "Comma", "No Comma" };
+	private static final String[] condition1Text = new String[] { "Filler", "Relative Clause", "Adverb", "Coordination", "Tutorial" };
+	public static final String[] condition2Text = new String[] { "Filler", "High Attachment", "Low Attachment", "Comma", "No Comma", "Tutorial" };
 
 	@Override
 	public String toString() {
